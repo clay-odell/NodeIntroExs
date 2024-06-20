a334521e637f4adc02f812c0d71b5f80e67cd953
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-19 21:01:41 -0500

    step 3 refactored

--- step3.js
+++ step3.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const process = require("process");
-const axios = require("axios");
+const axios = require('axios');
+
 
 function cat(path, output) {
   fs.readFile(path, "utf-8", function (err, data) {
@@ -8,48 +9,50 @@ function cat(path, output) {
       console.error(`Error reading ${path}:`, err);
       process.exit(1);
     }
-    if (output) {
-      fs.writeFile(output, data, (err) => {
-        if (err) {
-          console.error(`Error writing to ${output}:`, err);
-          process.exit(1);
-        }
-      });
-    } else {
-      console.log(data);
-    }
+    handleOutput(output, data);
   });
 }
+
 
 async function webCat(url, output) {
   try {
     const resp = await axios.get(url);
-    if (output) {
-      fs.writeFile(output, resp.data, (err) => {
-        if (err) {
-          console.error(`Error writing to ${output}:`, err);
-          process.exit(1);
-        }
-      });
-    } else {
-      console.log(resp.data);
-    }
+    handleOutput(output, resp.data);
   } catch (error) {
     console.error(`Error fetching ${url}:`, error);
     process.exit(1);
   }
 }
 
-let pathOrUrl, output;
-if (process.argv[2] === "--out") {
-  output = process.argv[3];
-  pathOrUrl = process.argv[4];
-} else {
-  pathOrUrl = process.argv[2];
+
+function handleOutput(output, data) {
+  if (output) {
+    fs.writeFile(output, data, (err) => {
+      if (err) {
+        console.error(`Error writing to ${output}:`, err);
+        process.exit(1);
+      }
+    });
+  } else {
+    console.log(data);
+  }
 }
 
-if (pathOrUrl.startsWith("http://") || pathOrUrl.startsWith("https://")) {
-  webCat(pathOrUrl, output);
-} else {
-  cat(pathOrUrl, output);
+
+function main() {
+  let pathOrUrl, output;
+  if (process.argv[2] === '--out') {
+      output = process.argv[3];
+      pathOrUrl = process.argv[4];
+  } else {
+      pathOrUrl = process.argv[2];
+  }
+
+  if (pathOrUrl.startsWith('http://') || pathOrUrl.startsWith('https://')) {
+      webCat(pathOrUrl, output);
+  } else {
+      cat(pathOrUrl, output);
+  }
 }
+
+main();
