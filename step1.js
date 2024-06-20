9214af9019f7995099036b6199a40d2266434bc9
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-19 19:09:53 -0500

    init commit and step 1 completed

---
+++ step1.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+    const process = require('process');
+function cat(path) {
+    fs.readFile(path, 'utf-8', function(err, data){
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log(data);
+    })
+}
+const path = process.argv[2];
+cat(path);
